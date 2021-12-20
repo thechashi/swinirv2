4c1e09ba0b2d76363f468ddd052ab106eb22bd35
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-12-20 12:18:44 -0800

    fix: no permutation

--- utils/utils_image.py
+++ utils/utils_image.py
@@ -312,8 +312,8 @@ def tensor2uint(img):
 
 # convert single (HxWxC) to 3-dimensional torch tensor
 def single2tensor3(img):
-    #return torch.from_numpy(np.ascontiguousarray(img)).float()
-    return torch.from_numpy(np.ascontiguousarray(img)).permute(2, 0, 1).float()
+    #return torch.from_numpy(np.ascontiguousarray(img)).permute(2, 0, 1).float()
+    return torch.from_numpy(np.ascontiguousarray(img)).float()
 
 
 # convert single (HxWxC) to 4-dimensional torch tensor
