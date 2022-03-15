9f2b8dc8d59fbdbd46d8806ccc35fc6fe37b2cd0
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2022-03-15 13:54:23 -0400

    feat: file name mismatch

--- utils/test_f2g.py
+++ utils/test_f2g.py
@@ -44,7 +44,7 @@ def test(model_path, input_path):
         cv2.imwrite("before_normalization.jpg", np.array(image))
         image = torch.tensor(image).reshape((1,1,256,256))
         image = (image/300890.0)*3
-        cv2.imwrite("before_normalization.jpg", np.array(image[0][0].to("cpu")))
+        cv2.imwrite("after_normalization.jpg", np.array(image[0][0].to("cpu")))
         image = image.to(device)
         output = model(image)
         print(output.shape)
